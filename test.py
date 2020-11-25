1202a741c1901d37f7f0a03006524b3a0a753704
Hongkun Xu <birdtianyu@gmail.com>
2020-11-25 14:46:48 +0900

    Update test.py

--- test.py
+++ test.py
@@ -5,11 +5,16 @@
 # 
 
 import tensorflow as tf
+import random
 import numpy as np
+import matplotlib.pyplot as plt
 
-mnist = tf.keras.datasets.mnist
+# mnist = tf.keras.datasets.mnist
+fashion_mnist = tf.keras.datasets.fashion_mnist
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+(x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
 x_train, x_test = x_train / 255.0, x_test / 255.0
 
 
@@ -28,3 +33,47 @@ model.compile(optimizer='adam',
 model.fit(x_train, y_train, epochs=5)
 
 model.evaluate(x_test,  y_test, verbose=2)
+
+def plot_image(i, predictions_array, true_label, img):
+  predictions_array, true_label, img = predictions_array, true_label[i], img[i]
+  plt.grid(False)
+  plt.xticks([])
+  plt.yticks([])
+
+  plt.imshow(img, cmap=plt.cm.binary)
+
+  predicted_label = np.argmax(predictions_array)
+  if predicted_label == true_label:
+    color = 'blue'
+  else:
+    color = 'red'
+
+  plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+                                100*np.max(predictions_array),
+                                class_names[true_label]),
+                                color=color)
+
+def plot_value_array(i, predictions_array, true_label):
+  predictions_array, true_label = predictions_array, true_label[i]
+  plt.grid(False)
+  plt.xticks(range(10))
+  plt.yticks([])
+  thisplot = plt.bar(range(10), predictions_array, color="#777777")
+  plt.ylim([0, 1])
+  predicted_label = np.argmax(predictions_array)
+
+  thisplot[predicted_label].set_color('red')
+  thisplot[true_label].set_color('blue')
+
+number = random.randint(1,100)
+predictions = model.predict(x_test)
+x_train = x_train[..., np.newaxis]
+
+plt.figure(figsize=(6,3))
+plt.subplot(1,2,1)
+plot_image(number, predictions[number], y_test, x_test)
+plt.subplot(1,2,2)
+plot_value_array(number, predictions[number],  y_test)
+
+plt.savefig('./image.png',bbox_inches='tight')
+plt.show()
